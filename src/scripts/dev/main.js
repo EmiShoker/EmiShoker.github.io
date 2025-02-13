d1b43b521f9ed0a561920bfe7c1a9ae6b3c171cf
ryan <emishoks@gmail.com>
2025-02-13 21:35:16 +0700

    js features

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -262,6 +262,50 @@
             title: "TAGREE digital"
         });
     }
+
+    const jsSelectric = $(".js-selectric");
+
+    if (jsSelectric.length) {
+        jsSelectric.selectric({
+            nativeOnMobile: false
+        });
+    }
+
+    //phone pp
+
+    const MobileMask = $(".js-mobileMask");
+
+    if (MobileMask.length) {
+        MobileMask.mask("+7 (000) 000 00 00", { placeholder: "+7 (___) ___ __ __" });
+    }
+
+    //date
+
+    const dateField = $(".js-dateField");
+    if (dateField.length) {
+        const pickerInit = function (pick) {
+            const dateInput = pick.find(".js-dateInput");
+            const dateDay = pick.find(".js-dateDay");
+            const dateMonth = pick.find(".js-dateMonth");
+            const dateYear = pick.find(".js-dateYear");
+            const dateConfig = {
+                autoClose: true,
+                minDate: new Date(),
+                navTitles: {
+                    days: "MMMM <i>yyyy</i>"
+                },
+                onSelect: function ({ date }) {
+                    dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+                    dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+                    dateYear.val(date ? date.getFullYear() : "");
+                }
+            };
+            new AirDatepicker(dateInput[0], dateConfig);
+        };
+        $.each(dateField, function (i) {
+            pickerInit($(this));
+        });
+    }
 })();
 
 //if (eventPP) {
